bcfed8bf1d26a22c5f66ed6c4509cede03ecea3a
Erik LaBianca <erik.labianca@gmail.com>
2016-01-23 21:27:52 -0500

    Add node.availability zone property from aws instance metadata

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,8 +15,9 @@ if [ "$1" = 'elasticsearch' ]; then
 fi
 
 # ECS will report the docker interface without help, so we override that with host's private ip
+AWS_AVAILABILITY_ZONE=`curl http://169.254.169.254/latest/meta-data/placement/availability-zone`k
 AWS_PRIVATE_IP=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-set -- "$@" --network.publish_host=$AWS_PRIVATE_IP
+set -- "$@" --network.publish_host=$AWS_PRIVATE_IP --node.availability-zone=$AWS_AVAILABILITY_ZONE
 
 # As argument is not related to elasticsearch,
 # then assume that user wants to run his own process,
